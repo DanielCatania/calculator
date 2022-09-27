fb39ba34e42abe9e52fbe57569727576bba9ddd1
Daniel Catania <dcataniarodrigues20@gmail.com>
2022-09-27 20:40:47 -0300

    add size and background on screen

--- src/components/Calc/Screen/BoxScreen.ts
+++ src/components/Calc/Screen/BoxScreen.ts
@@ -1,12 +1,15 @@
 import styled from "styled-components";
 
 const BoxScreen = styled.div`
+  border-radius: 15px;
+  width: 70%;
   padding: 1em;
   padding-top: 2em;
-  border-radius: 15px;
 
   display: flex;
   justify-content: flex-end;
+
+  background-color: #fff;
 `;
 
 export default BoxScreen;
