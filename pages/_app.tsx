953af99b573688f95e30837a22b09a3b7ffc1871
Daniel Catania <dcataniarodrigues20@gmail.com>
2022-09-27 19:46:59 -0300

    add reset css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import { AppProps } from "next/app";
 import React from "react";
+import ResetCSS from "../src/style/ResetCSS";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <ResetCSS />
+    </>
+  );
 }
 
 export default MyApp;
